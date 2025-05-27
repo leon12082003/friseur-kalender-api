6fe5f242ef2db789eedcf088f9bcf98afbb56239
leon12082003 <markus.bringer@gmx.de>
2025-05-27 20:15:21 +0200

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class TerminAnfrage(BaseModel):
+    name: str
+    friseur: str
+    datum: str
+    uhrzeit: str
+    bemerkung: Optional[str] = None
+
+class TerminAbsage(BaseModel):
+    name: str
+    friseur: str
+    datum: str
+    uhrzeit: str
+
+class TerminVerschiebung(BaseModel):
+    name: str
+    friseur: str
+    alt_datum: str
+    alt_uhrzeit: str
+    neu_datum: str
+    neu_uhrzeit: str
+
+class FreieZeitenAnfrage(BaseModel):
+    friseur: str
+    datum: Optional[str] = None  # NEU
+
+class KombiPerson(BaseModel):
+    name: str
+    friseur: str
+    datum: Optional[str]
+    uhrzeit: Optional[str]
+    bemerkung: Optional[str]
+
+class KombiBuchung(BaseModel):
+    kunden: List[KombiPerson]
