70ffafa2664fbb53866b33d4a3cc6c2fc949ee35
leon12082003 <markus.bringer@gmx.de>
2025-05-27 14:02:12 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,10 +8,10 @@ OPENING_HOURS = {
 
 # Kalender-IDs (Platzhalter – bitte durch echte IDs ersetzen)
 CALENDAR_IDS = {
-    "Karin Meier": "kalender_karin_id@google.com",
-    "Marco Hauer": "kalender_marco_id@google.com",
-    "Lisa Fischer": "kalender_lisa_id@google.com",
-    "Max Zimmer": "kalender_max_id@google.com"
+    "Karin Meier": "1e3a743874cf760bf08026e4fd7eb4a2692c5996e8de1181fd8c87837be524e7@group.calendar.google.com",
+    "Marco Hauer": "00edba1ec002f208f25bbd78a7649ef0518121d31071a4fed10ecc0393e3d4b9@group.calendar.google.com",
+    "Lisa Fischer": "c196fca542ff5176c62b11805596e015f5fb2aff1ec0b73c43cf555937911638@group.calendar.google.com",
+    "Max Zimmer": "5b679eaf3a0999b06265e6b21dd2200800e8b2338056475e1cbd8f17ab34f861@group.calendar.google.com"
 }
 
 # Mitarbeiterprofile
